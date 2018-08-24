17fe1f4f3c00093b9e016158c7a06f0388b6f418
DESKTOP-EEAGJBJ\allen <a15963lleoallentw@gmail.com>
2018-08-24 14:09:52 +0800

    create

---
+++ Tools Development/Tools Development/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tools_Development
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button checkButton = new Button();
+            checkButton.Name = "checkButton";
+            checkButton.Click += Check;
+
+        }
+
+        private void Check(object sender, RoutedEventArgs e)
+        {
+            JObject o1 = JObject.Parse(File.ReadAllText(@"c:\videogames.json"));
+        }
+    }
+}
