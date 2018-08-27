64c7a2a416d8849f68e69f9a7e6c30fe1efb4788
Joseph Lai <lai225joseph@hotmail.com>
2018-08-27 13:00:49 +0800

    algin horizontal

--- Tools Development/Tools Development/MainWindow.xaml.cs
+++ Tools Development/Tools Development/MainWindow.xaml.cs
@@ -27,29 +27,32 @@ namespace Tools_Development
 
         private void SetTextBox(List<Dictionary<string, string>> jsonSchema)
         {
-            //int teams = jsonSchema.Count;
-            //TextBox[] txtTeamNames = new TextBox[teams];
-            //string value = "";
-            //for (int i = 0; i < txtTeamNames.Length; i++)
-            //{
-            //    var jsonValue = jsonSchema[i].Values;
-            //    foreach (var item in jsonValue)
-            //    {
-            //        int start = item.IndexOf('[');
-            //        int end = item.LastIndexOf(']');
-            //        value = item.Substring(start + 1, end - start - 1);
-            //    }
-            //    var txt = new TextBox();
-            //    txtTeamNames[i] = txt;
-            //    txt.Width = Convert.ToInt32(value) * 10;
-            //    txt.Height = 50;
-            //    txt.Margin = new Thickness(-250 + i /5 * 35, 0 + 50 * (i % 5), 0, 0);
-            //    txt.BorderThickness = new Thickness(5, 5, 5, 5);
-            //    toolsLine.Children.Add(txt);
-            //}
-
-
+            int startLocation = -600;
             int location = 0;
+            int textboxLocation = 0;
+
+            int teams = jsonSchema.Count;
+            TextBox[] txtTeamNames = new TextBox[teams];
+            string text = "";
+            for (int i = 0; i < txtTeamNames.Length; i++)
+            {
+                var jsonValue = jsonSchema[i].Values;
+                foreach (var item in jsonValue)
+                {
+                    int start = item.IndexOf('[');
+                    int end = item.LastIndexOf(']');
+                    text = item.Substring(start + 1, end - start - 1);
+                }
+                var txt = new TextBox();
+                txtTeamNames[i] = txt;
+                txtTeamNames[i].HorizontalAlignment = HorizontalAlignment.Left;
+                txtTeamNames[i].Width = Convert.ToInt32(text) * 10;
+                txtTeamNames[i].Height = 50;
+                txtTeamNames[i].Margin = new Thickness(200, textboxLocation + startLocation, 0, 0);
+                textboxLocation += 38;
+                showGrid.Children.Add(txtTeamNames[i]);
+            }
+
             foreach (var listDictionary in jsonSchema)
             {
                 foreach (string key in listDictionary.Keys)
@@ -61,10 +64,10 @@ namespace Tools_Development
                     dynamicLabel.Content = key;
                     dynamicLabel.Width = 240;
                     dynamicLabel.Height = 30;
-                    dynamicLabel.Margin = new Thickness(0, location, 0, 0);
-                    dynamicLabel.Foreground = new SolidColorBrush(Colors.White);
-                    dynamicLabel.Background = new SolidColorBrush(Colors.Black);
-                    location += 34;
+                    dynamicLabel.Margin = new Thickness(0, location + startLocation, 0, 0);
+                    //dynamicLabel.Foreground = new SolidColorBrush(Colors.White);
+                    //dynamicLabel.Background = new SolidColorBrush(Colors.Black);
+                    location += 38;
                     showGrid.Children.Add(dynamicLabel);
                 }
             }
@@ -98,7 +101,8 @@ namespace Tools_Development
         {
             //C:\Users\allen\Documents\ToolsDevelopment\Tools Development\Tools Development\assets\json\schema.json
             //string position = textBox.Text;
-            string position = @"C:\Users\allen\Documents\ToolsDevelopment\Tools Development\Tools Development\assets\json\schema.json";
+            //string position = @"C:\Users\allen\Documents\ToolsDevelopment\Tools Development\Tools Development\assets\json\schema.json";
+            string position = @"D:\Visual Studio\ToolsDevelopmentNew\Tools Development\Tools Development\assets\json\schema.json";
             if (System.IO.File.Exists(position))
             {
                 using (StreamReader r = new StreamReader(@position))
